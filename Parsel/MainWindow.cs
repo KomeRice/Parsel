d3d866aa0751b729f35f744ea6449b65b2a00344
Rice <golde150@gmail.com>
2020-12-10 16:40:39 +0100

    Base highlighting

--- Parsel/MainWindow.cs
+++ Parsel/MainWindow.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Security.Cryptography;
 using Gdk;
 using Gtk;
+using Pango;
 using UI = Gtk.Builder.ObjectAttribute;
 using Window = Gtk.Window;
 
@@ -15,6 +16,11 @@ namespace Parsel
 	{
 		[UI] private Label _labelPrompt = null;
 		[UI] private readonly Button _buttonPickFile = null;
+		
+		[UI] private TextView _printTrace = null;
+		[UI] private TreeView _displayTrace = null;
+		[UI] private TextBuffer _traceBuffer = null;
+		[UI] private ListStore _traceTree = null;
 
 		public MainWindow() : this(new Builder("MainWindow.glade"))
 		{
@@ -38,34 +44,32 @@ namespace Parsel
 			var fc = new FileChooserDialog("Choose file to open", this, 
 				FileChooserAction.Open, "Cancel", ResponseType.Cancel, "Open", ResponseType.Accept);
 
+			var filter = new FileFilter {Name = "Fichiers texte"};
+			filter.AddPattern("*.txt");
+			fc.Filter = filter;
+
 			if (fc.Run() == (int) ResponseType.Accept)
 			{
-				var f = File.OpenRead(fc.Filename);
-				var hash = MD5.Create();
-				var hashValue = BitConverter.ToString(hash.ComputeHash(f));
-				var messageDialog = new MessageDialog(this,DialogFlags.DestroyWithParent,MessageType.Info,
-					ButtonsType.Close,$"Computed hash of {fc.File.Basename} is:\n{hashValue}");
-				f.Close();
+				var f = File.ReadAllText(fc.Filename);
 				fc.Dispose();
-				messageDialog.Run();
-				messageDialog.Dispose();
+				_printTrace.Buffer.Text = f;
+				var index = f.IndexOf("AMD", StringComparison.Ordinal);
 				TextTag hl = new TextTag("Highlight")
 				{
-					BackgroundRgba = new RGBA()
-					{
-						Red = 0,
-						Green = 0,
-						Blue = 255,
-						Alpha = 1
-					},
-					ForegroundRgba = new RGBA()
-					{
-						Red = 255,
-						Green = 255,
-						Blue = 255,
-						Alpha = 1
-					}
+					Background = "#0000ff",
+					Foreground = "white",
+					Weight = Weight.Bold
 				};
+
+				_traceBuffer.TagTable.Add(hl);
+				
+				var iter1 = _traceBuffer.GetIterAtOffset(index);
+				var iter2 = _traceBuffer.GetIterAtOffset(index + 3);
+				
+				Application.Invoke(delegate
+				{
+					_traceBuffer.ApplyTag(hl, iter1, iter2);
+				});
 			}
 			else
 			{
