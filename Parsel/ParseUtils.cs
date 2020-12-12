54ad920b1a1e42bd35a64231baf44f136d62e9f4
Rice <golde150@gmail.com>
2020-12-12 15:12:08 +0100

    ipv4 parse

--- Parsel/ParseUtils.cs
+++ Parsel/ParseUtils.cs
@@ -77,6 +77,157 @@ namespace Parsel
 			ethernetLayer.AddChild(srcIpRange);
 
 			return ethernetLayer;
+			
+			
+		}
+
+		public static ByteRange ParseIp(ByteRange packet, string data)
+		{
+			// ModelHelper.CursorSetter(packet.GetRangeStart(), data) + 48
+			
+			// Version
+			var firstByteAsList = new List<byte>() {packet.GetByteList()[14]};
+			var first = packet.GetByteList()[14].ToString("X2");
+			var version = Convert.ToInt32(first.Substring(0,1), 16);
+
+			// Header Length
+			var headerBytes = first.Substring(1,1);
+			var headerLength = Convert.ToInt32(headerBytes, 16) * 4;
+			
+			// Type of service
+			var tos = packet.GetByteList()[15].ToString("X2");
+			
+			// Total Length
+			var tLengthBytes = packet.GetByteList().GetRange(16, 2);
+			var totalLength = Convert.ToInt32(string.Join("", tLengthBytes.Select(b => b.ToString("X2"))), 16);
+			
+			// Identification
+			var identificationBytes = packet.GetByteList().GetRange(18, 2);
+			var identification = "0x" + string.Join("", identificationBytes.Select(b => b.ToString("X2")));
+			
+			// Fragmentation
+			var fragBytes = packet.GetByteList().GetRange(20, 2);
+			var frag = ToBinaryWord(Convert.ToInt32(string.Join("", fragBytes.Select(b => b.ToString("X2")))),16);
+			var flagsStr = "0x" + string.Join("", fragBytes.Select(b => b.ToString("X2")));
+
+			
+			// Flags
+			var fragFlags = frag.Substring(0, 3);
+			var reserved = Convert.ToInt32(fragFlags.Substring(0,1));
+			var dontFrag = Convert.ToInt32(fragFlags.Substring(1,1));
+			var moreFrag = Convert.ToInt32(fragFlags.Substring(2,1));
+			
+			// Offset
+			var fragOffset = Convert.ToInt32(frag.Substring(3, 13), 2) * 8;
+
+			// TTL
+			var ttlByte = packet.GetByteList().GetRange(22, 1);
+			var ttl = Convert.ToInt32(ttlByte[0]);
+			
+			// Protocol
+			var protocolBytes = packet.GetByteList().GetRange(23,1);
+			var protocolStr = protocolBytes[0].ToString();
+			var protocol = Convert.ToInt32(protocolStr) switch
+			{
+				1 => "ICMP",
+				6 => "TCP",
+				17 => "UDP",
+				_ => "Unsupported"
+			};
+			
+			// Checksum
+			var checksumBytes = packet.GetByteList().GetRange(24, 2);
+			var checksum = "0x" + string.Join("", checksumBytes.Select(b => b.ToString("X2")));
+			
+			// Source Ip
+			var srcIpBytes = packet.GetByteList().GetRange(26, 4);
+			var srcIp = string.Join('.', srcIpBytes.Select(b => b.ToString()));
+			
+			// Source Ip
+			var dstIpBytes = packet.GetByteList().GetRange(30, 4);
+			var dstIp = string.Join('.', dstIpBytes.Select(b => b.ToString()));
+
+			// Make ByteRanges
+			
+			var cursor = ModelHelper.ByteShifter(packet.GetRangeStart(), data, 14);
+			
+			var ipv4 = new ByteRange("IPv4", cursor, ModelHelper.ByteShifter(cursor, data, headerLength), 
+				packet.GetByteList().GetRange(14,headerLength), $"Src: {srcIp}, Dst: {dstIp}");
+
+			var versionRange = new ByteRange("Version", cursor, ModelHelper.ByteShifter(cursor, data, 1), 
+				firstByteAsList, version.ToString());
+			ipv4.AddChild(versionRange);
+			
+			var ihlRange = new ByteRange("Header Length", cursor, ModelHelper.ByteShifter(cursor, data, 1),
+				firstByteAsList, $"Header Length: {headerLength.ToString()}");
+			ipv4.AddChild(ihlRange);
+
+			var tosRange = new ByteRange("Type of service", ihlRange.GetRangeEnd(), ModelHelper.ByteShifter(ihlRange.GetRangeEnd(), data, 1),
+				new List<byte>() {packet.GetByteList()[15]}, $"Type of service: {tos}");
+			ipv4.AddChild(tosRange);
+
+			var totalLengthRange = new ByteRange("Total Length", 
+				tosRange.GetRangeEnd(), ModelHelper.ByteShifter(tosRange.GetRangeEnd(), data, 2),
+				tLengthBytes,$"Total Length: {totalLength} bytes");
+			ipv4.AddChild(totalLengthRange);
+
+			var identificationRange = new ByteRange("Identification",
+				totalLengthRange.GetRangeEnd(), ModelHelper.ByteShifter(totalLengthRange.GetRangeEnd(), data, 2),
+				identificationBytes, $"Identification: {identification}");
+			ipv4.AddChild(identificationRange);
+
+			var flags = new ByteRange("Flags: ",
+				identificationRange.GetRangeEnd(), ModelHelper.ByteShifter(identificationRange.GetRangeEnd(), data, 2),
+				fragBytes, $"{flagsStr}");
+			ipv4.AddChild(flags);
+
+			var reservedFlagRange = new ByteRange($"{reserved}... .... .... ....",
+				identificationRange.GetRangeEnd(), ModelHelper.ByteShifter(identificationRange.GetRangeEnd(), data, 2),
+				fragBytes, $"Reserved Bit: {(reserved == 1 ? "Set" : "Not set")}");
+
+			var dontFragRange = new ByteRange($".{dontFrag}.. .... .... ....",
+				identificationRange.GetRangeEnd(), ModelHelper.ByteShifter(identificationRange.GetRangeEnd(), data, 2),
+				fragBytes, $"Don't fragment: {(dontFrag == 1 ? "Set" : "Not set")}");
+
+			var moreFragRange = new ByteRange($"..{moreFrag}. .... .... ....",
+				identificationRange.GetRangeEnd(), ModelHelper.ByteShifter(identificationRange.GetRangeEnd(), data, 2),
+				fragBytes, $"More fragments: {(moreFrag == 1 ? "Set" : "Not set")}");
+
+			flags.AddChild(reservedFlagRange);
+			flags.AddChild(dontFragRange);
+			flags.AddChild(moreFragRange);
+			
+			var fragOffRange = new ByteRange("Fragment offset", 
+				identificationRange.GetRangeEnd(), ModelHelper.ByteShifter(identificationRange.GetRangeEnd(), data, 2),
+				fragBytes, fragOffset.ToString());
+			ipv4.AddChild(fragOffRange);
+
+			var ttlRange = new ByteRange("Time to live",
+				fragOffRange.GetRangeEnd(), ModelHelper.ByteShifter(fragOffRange.GetRangeEnd(), data, 1),
+				ttlByte, ttl.ToString());
+			ipv4.AddChild(ttlRange);
+
+			var protocolRange = new ByteRange("Protocol",
+				ttlRange.GetRangeEnd(), ModelHelper.ByteShifter(ttlRange.GetRangeEnd(), data, 1),
+				protocolBytes, $"{protocol} ({protocolStr})");
+			ipv4.AddChild(protocolRange);
+
+			var headerChecksumRange = new ByteRange("Header checksum",
+				protocolRange.GetRangeEnd(), ModelHelper.ByteShifter(protocolRange.GetRangeEnd(), data, 2),
+				checksumBytes, checksum);
+			ipv4.AddChild(headerChecksumRange);
+
+			var srcIpRange = new ByteRange("Source IP",
+				headerChecksumRange.GetRangeEnd(), ModelHelper.ByteShifter(headerChecksumRange.GetRangeEnd(), data, 4),
+				srcIpBytes, srcIp);
+			ipv4.AddChild(srcIpRange);
+
+			var dstIpRange = new ByteRange("Destination IP",
+				srcIpRange.GetRangeEnd(), ModelHelper.ByteShifter(srcIpRange.GetRangeEnd(), data, 4),
+				dstIpBytes, dstIp);
+			ipv4.AddChild(dstIpRange);
+			
+			return ipv4;
 		}
 
 		public static IList<string> Format(string data)
